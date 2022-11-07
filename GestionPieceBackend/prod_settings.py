2cffcdd36ed190c6b3b6c2498f1b0af09edec949
KALMOGO <generaloks496@gmail.com>
2022-11-07 08:31:21 +0000

    settings updating

--- GestionPieceBackend/prod_settings.py
+++ GestionPieceBackend/prod_settings.py
@@ -7,7 +7,6 @@ DEBUG_TEMPLATE = False
 
 ALLOWED_HOSTS = ['backend-piece.herokuapp.com']
 
-SECRET_KEY = 'yl$&cyx^ogn7e+1l0n8dh!9nd&wpyuw_l8t_^d!138we87u3o7'
 
 DATABASES['default'] = dj_database_url.config()
 
@@ -15,3 +14,6 @@ MIDDLEWARE = [
    
     "whitenoise.middleware.WhiteNoiseMiddleware"
 ]
+
+
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
