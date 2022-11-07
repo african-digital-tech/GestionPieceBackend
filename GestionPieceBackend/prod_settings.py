dab7cc72be698def31f5a4eb9dfc939001fa8576
KALMOGO <generaloks496@gmail.com>
2022-11-07 01:10:21 +0000

    last step

--- GestionPieceBackend/prod_settings.py
+++ GestionPieceBackend/prod_settings.py
@@ -10,3 +10,8 @@ ALLOWED_HOSTS = ['backend-piece.herokuapp.com']
 SECRET_KEY = 'yl$&cyx^ogn7e+1l0n8dh!9nd&wpyuw_l8t_^d!138we87u3o7'
 
 DATABASES['default'] = dj_database_url.config()
+
+MIDDLEWARE = [
+   
+    "whitenoise.middleware.WhiteNoiseMiddleware"
+]
