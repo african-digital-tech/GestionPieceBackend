dee06006dfc737836d19eb429b031e3bba3bf107
KALMOGO <generaloks496@gmail.com>
2022-11-07 08:37:23 +0000

    prod_settings updating

--- GestionPieceBackend/prod_settings.py
+++ GestionPieceBackend/prod_settings.py
@@ -10,6 +10,8 @@ ALLOWED_HOSTS = ['backend-piece.herokuapp.com']
 
 DATABASES['default'] = dj_database_url.config()
 
+SECRET_KEY = "yl$&cyx^ogn7e+1l0n8dh!9nd&wpyuw_l8t_^d!138we87u3o7"
+
 MIDDLEWARE = [
    
     "whitenoise.middleware.WhiteNoiseMiddleware"
