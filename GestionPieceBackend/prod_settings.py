61463c08f50686a047e85a4bc72088bf9f1a68eb
KALMOGO <generaloks496@gmail.com>
2022-11-07 09:47:31 +0000

    middlewares adding in prod_settings

--- GestionPieceBackend/prod_settings.py
+++ GestionPieceBackend/prod_settings.py
@@ -13,7 +13,14 @@ DATABASES['default'] = dj_database_url.config()
 SECRET_KEY = "yl$&cyx^ogn7e+1l0n8dh!9nd&wpyuw_l8t_^d!138we87u3o7"
 
 MIDDLEWARE = [
-   
+    "django.middleware.security.SecurityMiddleware",
+    "django.contrib.sessions.middleware.SessionMiddleware",
+    "django.middleware.common.CommonMiddleware",
+    "django.middleware.csrf.CsrfViewMiddleware",
+    "django.contrib.auth.middleware.AuthenticationMiddleware",
+    "django.contrib.messages.middleware.MessageMiddleware",
+    "django.middleware.clickjacking.XFrameOptionsMiddleware",
+    'corsheaders.middleware.CorsMiddleware',
     "whitenoise.middleware.WhiteNoiseMiddleware"
 ]
 
