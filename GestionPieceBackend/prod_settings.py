eec0b52eadf19ab1f1537f671e5b145ee9a9f6bf
KALMOGO <generaloks496@gmail.com>
2022-11-07 00:57:08 +0000

    starting deployment ending

--- GestionPieceBackend/prod_settings.py
+++ GestionPieceBackend/prod_settings.py
@@ -9,4 +9,4 @@ ALLOWED_HOSTS = ['backend-piece.herokuapp.com']
 
 SECRET_KEY = 'yl$&cyx^ogn7e+1l0n8dh!9nd&wpyuw_l8t_^d!138we87u3o7'
 
-DATABASES = dj_database_url.config()
+DATABASES['default'] = dj_database_url.config()
