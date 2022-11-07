cf4476f38915779330d9304faa00b4508c6f7928
KALMOGO <generaloks496@gmail.com>
2022-11-07 00:35:59 +0000

    starting deployment

---
+++ GestionPieceBackend/prod_settings.py
@@ -0,0 +1,12 @@
+from .settings import *
+import dj_database_url
+
+
+DEBUG = False
+DEBUG_TEMPLATE = False
+
+ALLOWED_HOSTS = ['*']
+
+SECRET_KEY = 'yl$&cyx^ogn7e+1l0n8dh!9nd&wpyuw_l8t_^d!138we87u3o7'
+
+DATABASES = dj_database_url.config()
