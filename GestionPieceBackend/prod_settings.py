d5dafd852f7592eeb2e76be0a89cdd09e54623b7
KALMOGO <generaloks496@gmail.com>
2022-11-07 00:52:22 +0000

    adding prod_settings

--- GestionPieceBackend/prod_settings.py
+++ GestionPieceBackend/prod_settings.py
@@ -5,7 +5,7 @@ import dj_database_url
 DEBUG = False
 DEBUG_TEMPLATE = False
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['backend-piece.herokuapp.com']
 
 SECRET_KEY = 'yl$&cyx^ogn7e+1l0n8dh!9nd&wpyuw_l8t_^d!138we87u3o7'
 
